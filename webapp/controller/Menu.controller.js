00e7c46ac6ac319c60b371e179d00a9251a18168
Mickaël TAUCH <mtauch@augustareeves.fr>
2016-05-24 08:00:33 +0000

    change Login.view.xml add Menu.view.xml, Menu.controller.js

---
+++ webapp/controller/Menu.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("fr.ar.cia.controller.Menu", {
+
+		onInit: function() {
+		}
+	});
+
+});
