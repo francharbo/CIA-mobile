d749f6cb2bc0fe5f586108cdabe78b972fc4f294
Mickaël TAUCH <mtauch@augustareeves.fr>
2016-05-31 14:52:17 +0000

    Add image size

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -11,6 +11,20 @@ sap.ui.define([
 			this.setModel(this.getOwnerComponent().getModel("login"));
 		},
 		
+		onAfterRendering: function() {
+			var header = this.byId("header");
+			var headerLength = header.getHeight().length;
+			var textLogo = this.byId("textLogo");
+			textLogo.setHeight($(window).height()*header.getHeight().substring(0, headerLength-1)/100+ "px");
+			var phoneLogo = this.byId("phoneLogo");
+			phoneLogo.setHeight($(window).height()*header.getHeight().substring(0, headerLength-1)/100+ "px");
+			
+			var footer = this.byId("footer");
+			var footerLength = footer.getHeight().length;
+			var avrilLogo = this.byId("avrilLogo");
+			avrilLogo.setHeight($(window).height()*footer.getHeight().substring(0, footerLength-1)/100+ "px");
+		},
+		
 		onLog: function() {
 			var login = this.byId("collabInput").getValue();
 			var password = this.byId("pwd").getValue();
@@ -25,7 +39,7 @@ sap.ui.define([
 				}
 			}
 			
-			sap.ui.commons.MessageBox.alert("Hello World from MessageBox.alert()!");	
+			sap.ui.commons.MessageBox.alert("Login/Mot de passe incorrect");	
 			
 		}
 	});
--- webapp/controller/Menu.controller.js
+++ webapp/controller/Menu.controller.js
@@ -10,6 +10,19 @@ var that;
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.getRoute("menu").attachPatternMatched(this._onObjectMatched, this);
 		},
+		onAfterRendering: function() {
+			var header = this.byId("header");
+			var headerLength = header.getHeight().length;
+			var textLogo = this.byId("textLogo");
+			textLogo.setHeight($(window).height()*header.getHeight().substring(0, headerLength-1)/100+ "px");
+			var phoneLogo = this.byId("phoneLogo");
+			phoneLogo.setHeight($(window).height()*header.getHeight().substring(0, headerLength-1)/100+ "px");
+			
+			var footer = this.byId("footer");
+			var footerLength = footer.getHeight().length;
+			var avrilLogo = this.byId("avrilLogo");
+			avrilLogo.setHeight($(window).height()*footer.getHeight().substring(0, footerLength-1)/100+ "px");
+		},
 		_onObjectMatched: function(oEvent) {
 			that.usr = oEvent.getParameter("arguments").usrNumber;
 		},
